e4cc41ac6ac38dbe2cbf0aa0178c72f049e72d6e
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2023-06-01 14:18:17 +0300

    Image-galery
    image galery with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './style.css'
+
+
 
 function App() {
+
+  const images = [
+    "images/26-500x200.jpg",
+    "images/431-500x200.jpg",
+    "images/516-500x200.jpg",
+    "images/785-500x200.jpg",
+    "images/874-500x200.jpg"
+  ]
+
+  const [selectedImg,setSelectedImg] = useState(images[0])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <img src={selectedImg} alt="selected" className="selected"/>
+      </div>
+      <div className="imgContainer">
+        {images.map((img,index) => {
+          return <img key={index} src={img} alt="gallery" onClick={() => {
+            setSelectedImg(img)
+          }}/>
+        })}
+      </div>
+
     </div>
   );
 }
